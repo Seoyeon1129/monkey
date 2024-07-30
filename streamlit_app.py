b7319cae2002d857a34b7bd4645b3f77c4cfe16a
Seoyeon1129 <112914475+Seoyeon1129@users.noreply.github.com>
2024-07-30 17:48:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,6 @@ if is_valid_input and input_text:
         sample_area = st.empty()
         char_count_display = st.empty()
         text_area = st.empty()
-        match_found = False
 
         while st.session_state.running:
             st.session_state.char_count += 1
@@ -91,7 +90,10 @@ if is_valid_input and input_text:
             st.session_state.generated_text += new_char
 
             if st.session_state.generated_text[-input_length:] == input_text:
-                math_found=True
+                display_text = display_text.replace(input_text, f"<strong>{input_text}</strong>")
+                sample_area.write(f"입력한 텍스트: {input_text}")
+                char_count_display.write(f"현재까지 입력된 문자 개수: {st.session_state.char_count}")
+                text_area.markdown(f"<div style='word-wrap: break-word; word-break: break-all;'>{display_text}</div>", unsafe_allow_html=True)
                 st.success(f"입력한 텍스트 '{input_text}'와 동일한 문자열이 생성되었습니다!")
                 st.session_state.running = False
 
@@ -100,10 +102,8 @@ if is_valid_input and input_text:
             char_count_display.write(f"현재까지 입력된 문자 개수: {format(st.session_state.char_count,',')}")
             
             display_text = st.session_state.generated_text[-display_num:]
-            if match_found:
                 display_text = display_text.replace(input_text, f"<strong>{input_text}</strong>")
             text_area.markdown(f"<div style='word-wrap: break-word; word-break: break-all;'>{display_text}</div>", unsafe_allow_html=True)
-            char_count_display.write(f"현재까지 입력된 문자 개수: {st.session_state.char_count}")
             time.sleep(0.005)
 
     # 버튼 동작 설정
